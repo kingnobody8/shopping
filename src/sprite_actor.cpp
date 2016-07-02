8d47c1037cf140cdb3da4cb2344c2e436c2fb770
Matthew Johnson <hydraskillz@gmail.com>
2016-07-02 15:03:26 -0400

    got the dude walking around

--- src/sprite_actor.cpp
+++ src/sprite_actor.cpp
@@ -1,15 +1,42 @@
 #include "sprite_actor.h"
 #include "gfx_util.h"
 
-static sf::IntRect s_characterFrames[] =
+// [direction][frame]
+static sf::IntRect s_characterFrames[][3] =
 {
-	{0, 0, 32, 32},		// North
-	{64, 32, 32, 32},	// South
-	{32, 0, 32, 32},	// East
-	{0, 64, 32, 32},	// West
+	// North
+	{
+		{0, 0, 32, 32},
+		{64, 0, 32, 32},
+		{32, 96, 32, 32},
+	},
+
+	// South
+	{
+		{64, 32, 32, 32},
+		{64, 96, 32, 32},
+		{64, 64, 32, 32},
+	},
+	
+	// East
+	{
+		{32, 0, 32, 32},
+		{32, 64, 32, 32},
+		{32, 32, 32, 32},
+	},
+
+	// West
+	{
+		{0, 64, 32, 32},
+		{0, 96, 32, 32},
+		{0, 32, 32, 32},
+	},
 };
 
 SpriteActor::SpriteActor()
+	: m_frame(0)
+	, m_totalFrames(3)
+	, m_animTimer(0.0f)
 {
 	LoadTexture("assets/textures/prof.png", m_texture);
 	
@@ -22,34 +49,61 @@ SpriteActor::~SpriteActor()
 
 void SpriteActor::Update(float dt)
 {
-	float speed = 400.0f;
+	float speed = 32.0f;
+	bool moving = false;
+	sf::Vector2f vel;
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
-		m_sprite.move(-speed * dt, 0);
-
+		vel.x = -speed * dt;
+		vel.y = 0;
+		moving = true;
 		SetFacing(CharacterDirection::West);
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
-		m_sprite.move(speed * dt, 0);
-
+		vel.x = speed * dt;
+		vel.y = 0;
+		moving = true;
 		SetFacing(CharacterDirection::East);
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
-		m_sprite.move(0, -speed * dt);
-
+		vel.x = 0;
+		vel.y = -speed * dt;
+		moving = true;
 		SetFacing(CharacterDirection::North);
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 	{
-		m_sprite.move(0, speed * dt);
-
+		vel.x = 0;
+		vel.y = speed * dt;
+		moving = true;
 		SetFacing(CharacterDirection::South);
+	}
+
+	m_sprite.move(vel);
+
+	if (!moving)
+	{
+		m_frame = 0;
+		SetFacing(m_facing);
+	}
+	else
+	{
+		m_animTimer += dt;
+		if (m_animTimer > 0.16f)
+		{
+			m_animTimer = 0;
+			++m_frame;
+			if (m_frame == m_totalFrames)
+			{
+				m_frame = 0;
+			}
+		}
 	}
 }
 
@@ -60,5 +114,6 @@ void SpriteActor::Draw(sf::RenderWindow& window)
 
 void SpriteActor::SetFacing(CharacterDirection direction)
 {
-	m_sprite.setTextureRect(s_characterFrames[direction]);
+	m_sprite.setTextureRect(s_characterFrames[direction][m_frame]);
+	m_facing = direction;
 }
