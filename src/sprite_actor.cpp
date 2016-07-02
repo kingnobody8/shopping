5c6b8c295a768512a67fee1d92e0aaa64862b7fa
Matthew Johnson <hydraskillz@gmail.com>
2016-07-02 15:29:29 -0400

    character movement feels good now

--- src/sprite_actor.cpp
+++ src/sprite_actor.cpp
@@ -33,15 +33,30 @@ static sf::IntRect s_characterFrames[][3] =
 	},
 };
 
+static sf::Vector2i s_characterDeltas[] =
+{
+	{0, -1},	// North
+	{0, 1},		// South
+	{1, 0},		// East
+	{-1, 0},	// West
+	{0, 0},		// None
+};
+
 SpriteActor::SpriteActor()
 	: m_frame(0)
 	, m_totalFrames(3)
 	, m_animTimer(0.0f)
+	, m_speed(32.0f)
 {
 	LoadTexture("assets/textures/prof.png", m_texture);
 	
 	m_sprite.setTexture(m_texture);
 	SetFacing(CharacterDirection::North);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		m_keyTimes[i] = 0;
+	}
 }
 
 SpriteActor::~SpriteActor()
@@ -49,45 +64,27 @@ SpriteActor::~SpriteActor()
 
 void SpriteActor::Update(float dt)
 {
-	float speed = 32.0f;
-	bool moving = false;
-	sf::Vector2f vel;
+	CheckKey(sf::Keyboard::Left, CharacterDirection::West, dt);
+	CheckKey(sf::Keyboard::Right, CharacterDirection::East, dt);
+	CheckKey(sf::Keyboard::Up, CharacterDirection::North, dt);
+	CheckKey(sf::Keyboard::Down, CharacterDirection::South, dt);
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	float best = 10000.0f;
+	int b = CharacterDirection::Count;
+	for (int i = 0; i < CharacterDirection::Count; ++i)
 	{
-		vel.x = -speed * dt;
-		vel.y = 0;
-		moving = true;
-		SetFacing(CharacterDirection::West);
+		if (m_keyTimes[i] > 0 && m_keyTimes[i] < best)
+		{
+			best = m_keyTimes[i];
+			b = i;
+		}
 	}
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-	{
-		vel.x = speed * dt;
-		vel.y = 0;
-		moving = true;
-		SetFacing(CharacterDirection::East);
-	}
+	Move(s_characterDeltas[b].x, s_characterDeltas[b].y, dt);
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-	{
-		vel.x = 0;
-		vel.y = -speed * dt;
-		moving = true;
-		SetFacing(CharacterDirection::North);
-	}
+	m_sprite.move(m_velocity);
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-	{
-		vel.x = 0;
-		vel.y = speed * dt;
-		moving = true;
-		SetFacing(CharacterDirection::South);
-	}
-
-	m_sprite.move(vel);
-
-	if (!moving)
+	if (m_velocity.x == 0 && m_velocity.y == 0)
 	{
 		m_frame = 0;
 		SetFacing(m_facing);
@@ -116,4 +113,31 @@ void SpriteActor::SetFacing(CharacterDirection direction)
 {
 	m_sprite.setTextureRect(s_characterFrames[direction][m_frame]);
 	m_facing = direction;
+}
+
+void SpriteActor::CheckKey(sf::Keyboard::Key key, int i, float dt)
+{
+	if (sf::Keyboard::isKeyPressed(key))
+	{
+		m_keyTimes[i] += dt;
+	}
+	else
+	{
+		m_keyTimes[i] = 0;
+	}
+}
+
+void SpriteActor::Move(int x, int y, float dt)
+{
+	m_velocity.x = x * m_speed * dt;
+	m_velocity.y = y * m_speed * dt;
+
+	if (x != 0)
+	{
+		SetFacing(x < 0 ? CharacterDirection::West : CharacterDirection::East);
+	}
+	else if (y != 0)
+	{
+		SetFacing(y < 0 ? CharacterDirection::North : CharacterDirection::South);
+	}
 }
